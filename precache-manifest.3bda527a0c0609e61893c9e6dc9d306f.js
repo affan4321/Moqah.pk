e1e80c76821ecdf285448da1306ae8207c3ca13f
Muhammad Affan <affan4321@gmail.com>
2024-12-11 16:37:40 +0500

    Updates

---
+++ precache-manifest.3bda527a0c0609e61893c9e6dc9d306f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a372a4fe4bd9cf02ab7bc46e117004",
+    "url": "/Moqah.pk/index.html"
+  },
+  {
+    "revision": "0cfc70b801d27648a47d",
+    "url": "/Moqah.pk/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "5ca88e90e9860cfc7c11",
+    "url": "/Moqah.pk/static/css/main.2ad34fa7.chunk.css"
+  },
+  {
+    "revision": "0cfc70b801d27648a47d",
+    "url": "/Moqah.pk/static/js/2.62fd21ec.chunk.js"
+  },
+  {
+    "revision": "db1f095b436a4996bb126853725a7d4e",
+    "url": "/Moqah.pk/static/js/2.62fd21ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28a3138262c7d80af0a",
+    "url": "/Moqah.pk/static/js/3.390d5b0d.chunk.js"
+  },
+  {
+    "revision": "5ca88e90e9860cfc7c11",
+    "url": "/Moqah.pk/static/js/main.b35ba3ea.chunk.js"
+  },
+  {
+    "revision": "221a6033762463ae7a83",
+    "url": "/Moqah.pk/static/js/runtime-main.f10d01b0.js"
+  },
+  {
+    "revision": "5fd30ef569ae842579a3196bc2713438",
+    "url": "/Moqah.pk/static/media/bg1.5fd30ef5.png"
+  },
+  {
+    "revision": "ae8bfb881278bacf4c514984f17be77a",
+    "url": "/Moqah.pk/static/media/bg2.ae8bfb88.png"
+  },
+  {
+    "revision": "4789b7fe73e0408b04ee444217c7bac9",
+    "url": "/Moqah.pk/static/media/heroBg.4789b7fe.png"
+  },
+  {
+    "revision": "a797cc7b7e3764fc04b0d50bc8d15202",
+    "url": "/Moqah.pk/static/media/img.a797cc7b.png"
+  },
+  {
+    "revision": "499c6b3524c6b834b7d9f3a964ba604e",
+    "url": "/Moqah.pk/static/media/islamabad.499c6b35.jpg"
+  },
+  {
+    "revision": "e6f26e9907cb4cee54b76e9b016fc981",
+    "url": "/Moqah.pk/static/media/karachi.e6f26e99.png"
+  },
+  {
+    "revision": "f8999a755fcad8bd31d468ed41d447ac",
+    "url": "/Moqah.pk/static/media/lahore.f8999a75.jpg"
+  },
+  {
+    "revision": "e2c8a121ef28a748c000fb89e148042b",
+    "url": "/Moqah.pk/static/media/logoBig.e2c8a121.png"
+  }
+]);
